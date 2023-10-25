71a764e323b4ba9ef85209e99376e5f55555901f
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-25 13:10:06 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "subscriptionID" {
+    type = string
+    description = "Variable for our resource group"
+}
+
+variable "resourceGroupName" {
+    type = string
+    description = "name of resource group"
+}
+
+variable "location" {
+    type = string
+    description = "location of your resource group"
+}
+
+variable "clientID" {
+    type = string
+    description = "location of your resource group"
+}
+
+variable "clientSecret" {
+    type = string
+    description = "location of your resource group"
+}
+
+variable "subscriptionID" {
+    type = string
+    description = "location of your resource group"
+}
+
+variable "tenantID" {
+    type = string
+    description = "location of your resource group"
+}
+
