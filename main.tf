3657fc703d6f978706d43545e0d3a0fda51f8798
SamMilanese <44896496+SamMilanese@users.noreply.github.com>
2023-10-25 13:14:24 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+    version = "<=2.0.0"
+    subscription_id = var.subscriptionID
+    client_id = var.clientID
+    client_secret = var.clientSecret
+    tenant_id = var.tenantID
+
+    features {}
+}
+
+resource "azurerm_resource_group" "DevRG" {
+  name     = var.resourceGroupName
+  location = var.location
+
+  tags = {
+    environment = "Dev"
+  }
+}
